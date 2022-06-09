a2e9b850d8603d72b22e623c26e2c7dbc119b94f
alejox <alejoxdrums@gmail.com>
2022-06-08 20:41:58 -0500

    Fix autoprefix

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,19 +1,41 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { filterCategory } from '../store/slices/products.slice';
 
 const ProductDetail = () => {
 
     const [product, setProduct] = useState({});
 
+    const { id } = useParams();
+    const dispatch = useDispatch();
+
+    const products = useSelector(state => state.product)
 
     useEffect(() => {
-        axios.get('https://ecommerce-api-react.herokuapp.com/api/v1/products/1')
-            .then(res => setProduct(res.data))
-    },[])
-
+        axios.get(`https://ecommerce-api-react.herokuapp.com/api/v1/products/`)
+            .then(res => {
+                const productSearched = res.data.data.product.find(productItem => productItem.id === Number(id));
+                setProduct(productSearched);
+                dispatch(filterCategory(res.data.data.product.id));
+            });
+            
+    }, [id, dispatch]);
+    console.log(product);
     return (
-        <div>
-            <h2>Product Detail</h2>
+        <div className='container'>
+            <h2>{product.title}</h2>
+            <p>{product.category}</p>
+            <img src={product.productImgs} alt="" className='fluid-img w-100' />
+            <p>{product.description}</p>
+            <p><b>Price: </b> ${product.price}</p>
+
+            {
+                products?.map(productItem => (
+                    <li>{productItem.title}</li>
+                ))
+            }
         </div>
     );
 };
