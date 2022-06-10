01c32df14a7775e908861ffd38d1f5119015cfc4
alejox <alejoxdrums@gmail.com>
2022-06-09 23:08:05 -0500

    addToken

--- src/pages/Purchases.js
+++ src/pages/Purchases.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getPurchases } from '../store/slices/purchases.slice';
+
 
 const Purchases = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPurchases());
+    },[dispatch])
+
     return (
         <div>
             <h2>Purchases</h2>
