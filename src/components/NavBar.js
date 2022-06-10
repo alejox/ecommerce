5013e66e529d89916c596112903c73e429682885
alejox <alejoxdrums@gmail.com>
2022-06-10 01:26:39 -0500

    sidebar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,22 @@
-import React, {useEffect} from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
 import { getCart } from '../store/slices/cart.slices';
 import { useDispatch } from 'react-redux';
 
 const NavBar = () => {
 
     const logout = () => localStorage.setItem('token', '');
+    const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getCart());
-    },[dispatch]);
+    }, [dispatch]);
+
+
 
     return (
         <div>
@@ -20,13 +26,27 @@ const NavBar = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#/login">Login</Nav.Link>
                             <Nav.Link href="#/Purchases">Purchases</Nav.Link>
+                            <Nav.Link href="#/login">Login</Nav.Link>
+                            <Nav.Link role="button" onClick={handleShow}>Cart (Sidebar)</Nav.Link>
                             <Nav.Link role="button" onClick={logout}>Log out</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+            <Offcanvas show={show} onHide={handleClose} placement='end'>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Cart</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    Some text as placeholder. In real life you can have the elements you
+                    have chosen. Like, text, images, lists, etc.
+                </Offcanvas.Body>
+            </Offcanvas>
+
+
+
+
         </div>
     );
 };
